489fa3b196f9d00d7fe16c45b063d1f07440bcb2
jeffreybiles <bilesjeffrey@gmail.com>
2019-10-20 08:06:56 -0700

    Index page to VueX

--- store/index.js
+++ store/index.js
@@ -1,13 +1,32 @@
 export const state = () => ({
   videos: [],
-  currentVideo: {}
 })
 
 export const mutations = {
   SET_VIDEOS (state, videos) {
     state.videos = videos
   },
-  SET_CURRENT_VIDEO (state, video) {
-    state.currentVideo = video
+  SET_TAGS (state, tags) {
+    state.tags = tags
+  }
+}
+
+export const actions = {
+  async loadAllVideos({commit}){
+    let response = await this.$axios.get('/videos')
+    let videos = response.data.data;
+  
+    videos.forEach(v => {
+      v.attributes.tag_ids = v.relationships.tags.data.map(t => t.id);
+    });
+
+    commit('SET_VIDEOS', videos.map(v => v.attributes));
+  
+    let tags = response.data.included;
+    tags.forEach(t => {
+      t.attributes.id = t.id;
+    })
+
+    commit('SET_TAGS', tags.map(t => t.attributes))
   }
 }
