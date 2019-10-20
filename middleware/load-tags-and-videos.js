f2ca9ca42d446dec3b3335029841471fbe4e02e1
jeffreybiles <bilesjeffrey@gmail.com>
2019-10-20 09:45:30 -0700

    Only load on initial load

--- middleware/load-tags-and-videos.js
+++ middleware/load-tags-and-videos.js
@@ -1,4 +1,7 @@
-export default async function({store}) {
-  await store.dispatch('loadAllVideos');
-  await store.dispatch('loadAllTags');
+export default async function({store, from}) {
+  let isInitialLoad = !from;
+  if(isInitialLoad){
+    await store.dispatch('loadAllVideos');
+    await store.dispatch('loadAllTags');
+  }
 }
