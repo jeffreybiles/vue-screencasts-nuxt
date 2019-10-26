5635b5fe43d3a0c5f320646f9adee9485ebe7f0b
jeffreybiles <bilesjeffrey@gmail.com>
2019-10-26 11:30:49 -0700

    register working

--- pages/register.vue
+++ pages/register.vue
@@ -13,15 +13,15 @@
     },
     methods: {
       async registerUser(registrationInfo){
-        let response = await this.$store.dispatch('users/register', registrationInfo);
-        if(response.error){
-          this.$store.dispatch('snackbar/setSnackbar', {color: 'error', text: response.error});
-        } else {
+        try {
+          await this.$axios.post('/users', registrationInfo);
           await this.$auth.loginWith('local', {
             data: registrationInfo
           })
           this.$store.dispatch('snackbar/setSnackbar', {text: 'Welcome to our app, ' + this.$auth.user.name});
           this.$router.push("/");
+        } catch {
+          this.$store.dispatch('snackbar/setSnackbar', {color: 'error', text: response.error});
         }
       }
     }
