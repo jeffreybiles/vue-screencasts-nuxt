6a543c4a840cb767a46593fa6484b65dc605806f
jeffreybiles <bilesjeffrey@gmail.com>
2019-10-26 13:13:15 -0700

    Login and Register pages

---
+++ pages/login.vue
@@ -0,0 +1,26 @@
+<template>
+  <v-container>
+    <h1>Login</h1>
+
+    <UserAuthForm buttonText="Login" :submitForm="loginUser" />
+  </v-container>
+</template>
+
+<script>
+  import UserAuthForm from '@/components/UserAuthForm'
+  
+  export default {
+    components: {
+      UserAuthForm
+    },
+    methods: {
+      loginUser(logininfo){
+        alert('You pressed a button')
+      }
+    }
+  }
+</script>
+
+<style lang="scss" scoped>
+
+</style>
