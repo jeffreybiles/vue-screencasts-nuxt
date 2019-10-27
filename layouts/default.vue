3664f2e8198d987952eda28f37e8e03b06819aab
jeffreybiles <bilesjeffrey@gmail.com>
2019-10-26 18:57:22 -0700

    auth-admin middleware

--- layouts/default.vue
+++ layouts/default.vue
@@ -2,7 +2,9 @@
   <v-app>
     <v-app-bar app color="green">
       <v-btn text to="/">Vue Screencasts</v-btn>
-      <v-btn text to="/admin/videos">Admin</v-btn>
+      <v-btn text to="/admin/videos" v-if="$auth.user && $auth.user.admin">
+        Admin
+      </v-btn>
 
       <v-spacer />
       
--- pages/admin.vue
+++ pages/admin.vue
@@ -10,7 +10,7 @@
 
 <script>
   export default {
-    
+    middleware: ['auth-admin']
   }
 </script>
 
