ae949ca2936cc95f94ed4268cf1974e3ee1903ed
jeffreybiles <bilesjeffrey@gmail.com>
2019-10-31 12:12:32 -0700

    Make isInitialPageLoad work for more situations
    Using !from only works in some situations.  This one should work in all.

--- middleware/load-videos-and-tags.js
+++ middleware/load-videos-and-tags.js
@@ -1,7 +1,8 @@
 import RSVP from 'rsvp'
 
-export default async function({store, from}){
-  let isInitialPageLoad = !from;
+export default async function({store}){
+  let isInitialPageLoad = store.state.videos.videos.length == 0;
+  
   if(isInitialPageLoad){
     await RSVP.all([
       store.dispatch('videos/loadAll'),
