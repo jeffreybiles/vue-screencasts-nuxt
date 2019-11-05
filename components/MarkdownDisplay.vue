a564f72a0d29441e9260623438974a96240187d6
jeffreybiles <bilesjeffrey@gmail.com>
2019-11-05 10:12:35 -0800

    Highlighting with highlight.js

--- components/MarkdownDisplay.vue
+++ components/MarkdownDisplay.vue
@@ -4,11 +4,17 @@
 
 <script>
   import marked from 'marked';
-
+  import highlight from 'highlight.js';
+  import 'highlight.js/styles/solarized-dark.css';
+  
   export default {
     computed: {
       processedMarkdown(){
-        return marked(this.markdown);
+        return marked(this.markdown, {
+          highlight(md) {
+            return highlight.highlightAuto(md).value
+          }
+        });
       }
     },
     props: ['markdown']
@@ -16,5 +22,7 @@
 </script>
 
 <style lang="scss" scoped>
-
+  ::v-deep code:before {
+    content: "" !important;
+  }
 </style>
