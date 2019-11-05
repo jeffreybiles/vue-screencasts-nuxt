d338befafbb9b22cb4a0c56cd128baa4ae677f31
jeffreybiles <bilesjeffrey@gmail.com>
2019-11-05 13:07:16 -0800

    Improved UI for editing Markdown

--- components/VideoEditForm.vue
+++ components/VideoEditForm.vue
@@ -4,12 +4,28 @@
                   label="Name" 
                   counter=50
                   :rules="[required('name'), minLength('name', 5), maxLength('name', 50)]" />
-    <v-textarea v-model="video.description" 
-                label="Description" 
-                counter=true
-                :rules="[required('description'), minLength('description', 20)]"/>
-    <v-textarea v-model="video.code_summary"
-                label="Code Summary" />
+    <v-row>
+      <v-col cols="12" sm="6">
+        <v-textarea v-model="video.description" 
+                    label="Description" 
+                    counter=true
+                    rows="9"
+                    :rules="[required('description'), minLength('description', 20)]"/>
+      </v-col>
+      <v-col cols="12" sm="6">
+        <MarkdownDisplay :markdown="video.description" />
+      </v-col>
+    </v-row>
+    <v-row>
+      <v-col cols="12" sm="6">
+        <v-textarea v-model="video.code_summary"
+                    label="Code Summary" 
+                    rows="12" />
+      </v-col>
+      <v-col cols="12" sm="6">
+        <MarkdownDisplay :markdown="video.code_summary" />
+      </v-col>
+    </v-row>
     <v-text-field v-model="video.duration"
                   label="Duration (in seconds)" />
     <DurationDisplay :duration="video.duration" />
@@ -28,9 +44,12 @@
 <script>
   import validations from '@/utils/validations';
   import DurationDisplay from '@/components/DurationDisplay'
+  import MarkdownDisplay from '@/components/MarkdownDisplay';
+
   export default {
     components: {
-      DurationDisplay
+      DurationDisplay,
+      MarkdownDisplay
     },
     data() {
       return {
