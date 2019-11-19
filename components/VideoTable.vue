8bcaa72c66ef36307e06adf46f9b7e2aefa24791
jeffreybiles <bilesjeffrey@gmail.com>
2019-11-19 08:41:49 -0800

    Filter (including tags, but not including description)

--- components/VideoTable.vue
+++ components/VideoTable.vue
@@ -1,52 +1,64 @@
 <template>
-  <v-data-table :headers="headers"
-                sort-by="sortable_publish_date"
-                :sort-desc="true"
-                show-expand
-                :items="mungedVideos"
-                @click:row="clickAction">
-    <template #item.duration="{item}">
-      <DurationDisplay :duration="item.duration" />
-    </template>
-    <template #item.sortable_publish_date="{item}">
-      <DateDisplay :date="item.published_at" />
-    </template>
-    <template #item.tags="{item}">
-      <span v-for="tag_id in item.tag_ids" :key="tag_id">
-        <v-btn color="green lighten-2" 
-               class="mr-1"
-               x-small
-               :to="`/tags/${tag_id}`">
-          {{ getTag()(tag_id).name }}
-        </v-btn>
-      </span>
-    </template>
-    <template #expanded-item="{ headers, item }">
-      <td :colspan="headers.length">
-        <v-row>
-          <v-col cols="4">
-            <VideoWatch :video="item" />
-          </v-col>
-          <v-col cols="8">
-            <h1>{{item.name}}</h1>
-            <MarkdownDisplay :markdown="item.description" />
-          </v-col>
-        </v-row>
-      </td>
-    </template>
-    <template #item.played="{item}">
-      <div class="green--text" v-if="isPlayed()(item.id)">
-        <font-awesome-icon icon="check" />
-      </div>
-    </template>
-    <template #item.admin_actions="{item}">
-      <div class="actions">
-        <v-btn small :to="`/watch/${item.id}`">Watch</v-btn> 
-        <v-btn small :to="`/admin/videos/${item.id}/edit`">Edit</v-btn>
-        <v-btn small @click.stop="deleteVideo(item)">Delete</v-btn>
-      </div>
-    </template>
-  </v-data-table>
+  <div>
+    <div class="header">
+      <v-text-field
+        v-model="search"
+        label="Search"
+        single-line
+        hide-details
+      />
+    </div>
+    <v-data-table :headers="headers"
+                  sort-by="sortable_publish_date"
+                  :sort-desc="true"
+                  show-expand
+                  :items="mungedVideos"
+                  :search="search"
+                  @click:row="clickAction"
+                  :custom-filter="filter">
+      <template #item.duration="{item}">
+        <DurationDisplay :duration="item.duration" />
+      </template>
+      <template #item.sortable_publish_date="{item}">
+        <DateDisplay :date="item.published_at" />
+      </template>
+      <template #item.tags="{item}">
+        <span v-for="tag_id in item.tag_ids" :key="tag_id">
+          <v-btn color="green lighten-2" 
+                class="mr-1"
+                x-small
+                :to="`/tags/${tag_id}`">
+            {{ getTag()(tag_id).name }}
+          </v-btn>
+        </span>
+      </template>
+      <template #expanded-item="{ headers, item }">
+        <td :colspan="headers.length">
+          <v-row>
+            <v-col cols="4">
+              <VideoWatch :video="item" />
+            </v-col>
+            <v-col cols="8">
+              <h1>{{item.name}}</h1>
+              <MarkdownDisplay :markdown="item.description" />
+            </v-col>
+          </v-row>
+        </td>
+      </template>
+      <template #item.played="{item}">
+        <div class="green--text" v-if="isPlayed()(item.id)">
+          <font-awesome-icon icon="check" />
+        </div>
+      </template>
+      <template #item.admin_actions="{item}">
+        <div class="actions">
+          <v-btn small :to="`/watch/${item.id}`">Watch</v-btn> 
+          <v-btn small :to="`/admin/videos/${item.id}/edit`">Edit</v-btn>
+          <v-btn small @click.stop="deleteVideo(item)">Delete</v-btn>
+        </div>
+      </template>
+    </v-data-table>
+  </div>
 </template>
 
 <script>
@@ -54,6 +66,7 @@
   import DateDisplay from '@/components/DateDisplay'
   import VideoWatch from '@/components/VideoWatch'
   import MarkdownDisplay from '@/components/MarkdownDisplay'
+  import _ from 'lodash'
 
   import { mapGetters } from 'vuex'
 
@@ -63,7 +76,12 @@
       DateDisplay,
       VideoWatch,
       MarkdownDisplay
-    },    
+    },
+    data(){
+      return {
+        search: ""
+      }
+    },
     computed: {
       mungedVideos(){
         return this.videos.map(v => {
@@ -85,6 +103,14 @@
           this.$store.dispatch('videos/delete', video);
           this.$store.dispatch('snackbar/setSnackbar', {text: `You have successfully deleted your video, ${video.name}.`});
         }
+      },
+      filter(value, search, item) {
+        let matchesName = RegExp(search, 'i').test(item.name)
+        let tagMatches = item.tag_ids.map(id => {
+          return RegExp(search, 'i').test(this.getTag()(id).name)
+        })
+        let matchesTag = _.some(tagMatches)
+        return matchesName || matchesTag
       }
     },
     props: ['videos', 'headers', 'clickAction']
