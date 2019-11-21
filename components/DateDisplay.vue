028bd22a6be74d7c055d548abe81b7aeab98e235
jeffreybiles <bilesjeffrey@gmail.com>
2019-11-20 18:30:06 -0800

    Custom item (column) slots

---
+++ components/DateDisplay.vue
@@ -0,0 +1,15 @@
+<template>
+  <span v-if="date">
+    {{date.getMonth() + 1}}/{{date.getDate()}}/{{date.getFullYear()}}
+  </span>
+</template>
+
+<script>
+  export default {
+    props: ['date']
+  }
+</script>
+
+<style lang="scss" scoped>
+
+</style>
