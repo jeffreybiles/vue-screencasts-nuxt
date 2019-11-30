b2b6d3ff65bcc5a576ea3821fcc2314adb906b83
Jeffrey Biles <bilesjeffrey@gmail.comgit config -l>
2019-11-29 18:54:48 -0800

    Auth form submits upon hitting enter

--- components/UserAuthForm.vue
+++ components/UserAuthForm.vue
@@ -1,24 +1,26 @@
 <template>
   <v-form v-model="valid">
-    <v-text-field v-model="userInfo.name" 
-                  label="Name" 
-                  :rules="[required('name')]"
-                  v-if="hasName" />
+    <div @keydown.enter="submitForm(userInfo)">
+      <v-text-field v-model="userInfo.name" 
+                    label="Name" 
+                    :rules="[required('name')]"
+                    v-if="hasName" />
 
-    <v-text-field v-model="userInfo.email" 
-                  label="Email" 
-                  :rules="[required('email'), emailFormat()]"/>
+      <v-text-field v-model="userInfo.email" 
+                    label="Email" 
+                    :rules="[required('email'), emailFormat()]"/>
 
-    <v-text-field v-model="userInfo.password"
-                  label="Password"
-                  :type="showPassword ? 'text' : 'password'" 
-                  :append-icon="showPassword ? 'mdi-eye' : 'mdi-eye-off'"
-                  @click:append="showPassword = !showPassword"
-                  counter=true
-                  :rules="[required('password'), minLength('password', 8)]"
-                  />
+      <v-text-field v-model="userInfo.password"
+                    label="Password"
+                    :type="showPassword ? 'text' : 'password'" 
+                    :append-icon="showPassword ? 'mdi-eye' : 'mdi-eye-off'"
+                    @click:append="showPassword = !showPassword"
+                    counter=true
+                    :rules="[required('password'), minLength('password', 8)]"
+                    />
 
-    <v-btn @click="submitForm(userInfo)" :disabled="!valid">{{ buttonText }}</v-btn>
+      <v-btn @click="submitForm(userInfo)" :disabled="!valid">{{ buttonText }}</v-btn>
+    </div>
   </v-form>
 </template>
 
