7315a179cf694382e9e1865267fbe54e11c6b1e9
Jeffrey Biles <bilesjeffrey@gmail.comgit config -l>
2019-12-03 08:47:21 -0800

    Roadmap page.  No link to it yet.

---
+++ pages/roadmap.vue
@@ -0,0 +1,57 @@
+<template>
+  <v-container>
+    <h1>Roadmap</h1>
+
+    <h2>Mid to late December: Open Beta</h2>
+    <p>This open beta should be feature-complete for finding and watching videos.</p>
+    <p>Things could be a little rocky at first as I adjust based on user feedback.</p>
+    <p>As such, there will be a MAJOR discount for those who believe in the project this early.</p>
+
+    TODO
+    <ul>
+      <li>Finish Courses feature</li>
+      <li>Finish Email-related features</li>
+      <li>Upload all update introductions and conclusions</li>
+      <li>Intersperse MirageJS videos where appropriate</li>
+      <li>Assign videos to courses</li>
+      <li>Assign tags where appropriate</li>
+      <li>Attach domain, connect analytics, add privacy policy, basic SEO stuff, etc.</li>
+    </ul>
+
+    <h2>Early to mid January: Launch</h2>
+
+    <p>At this point we'll be ready for prime time. Prices raise, and I'll start releasing a new video most workdays.</p>
+    
+    TODO
+    <ul>
+      <li>Payment features</li>
+      <li>Update current videos based on user feedback</li>
+      <li>Update UI based on user feedback</li>
+      <li>Fix bugs found by users</li>
+      <li>Plan first few weeks of videos</li>
+    </ul>
+
+    <h2>After launch</h2>
+    <p>I'll be releasing new videos most workdays, and steadily improving the user experience.</p>
+
+    TODO
+    <ul>
+      <li>Refresh visual design</li>
+      <li>Improve thumbnails for courses</li>
+      <li>"Watch Later" list</li>
+      <li>Team Admin accounts</li>
+      <li>Bugfixes, video fixes, and UI improvements</li>
+      <li>Some really ambitious and useful new features you're gonna love :)</li>
+    </ul>
+  </v-container>
+</template>
+
+<script>
+  export default {
+    
+  }
+</script>
+
+<style lang="scss" scoped>
+
+</style>
