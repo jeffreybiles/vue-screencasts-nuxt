c87f25ae0d2f9b54688d5533ffa574525c3bdbef
Jeffrey Biles <bilesjeffrey@gmail.comgit config -l>
2019-12-07 18:23:26 -0800

    T&C + Privacy Policy

--- layouts/default.vue
+++ layouts/default.vue
@@ -23,6 +23,25 @@
       <nuxt />
     </v-content>
 
+    <v-footer padless>
+      <v-card
+        flat
+        tile
+        width="100%"
+        class="text-center"
+      >
+        <v-card-text>
+          <a href="https://www.websitepolicies.com/policies/view/Yz6Uu8KI" target="_blank" rel="nofollow">
+            Terms and Conditions
+          </a>
+          <a href="https://www.websitepolicies.com/policies/view/ExoXOmAv" target="_blank" rel="nofollow">
+            Privacy Policy
+          </a>
+          Policies last updated December 7, 2019
+        </v-card-text>
+      </v-card>
+    </v-footer>
+
     <v-snackbar
       v-for="(snackbar, index) in snackbars.filter(s => s.showing)"
       :key="snackbar.text + Math.random()"
