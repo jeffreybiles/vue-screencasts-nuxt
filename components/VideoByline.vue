30d2b900b6ce368fd9fa8b4d53ecf77b90ea4a4f
Jeffrey Biles <bilesjeffrey@gmail.comgit config -l>
2019-12-08 11:42:59 -0800

    Show part # and coures in video byline

--- components/VideoByline.vue
+++ components/VideoByline.vue
@@ -8,22 +8,39 @@
     <span v-else>
       This video is not yet published
     </span>
+    <span v-if="video.course_id">
+      &nbsp; | &nbsp;
+      Part {{ partNumber }} of {{ course.name }}
+    </span>
   </em>
 </template>
 
 <script>
   import DurationDisplay from '@/components/DurationDisplay'
   import DateDisplay from '@/components/DateDisplay'
-  
+  import { mapGetters } from 'vuex'
+  import { sortCourse } from '@/utils/course-decorator.js';
+
   export default {
     components: {
       DurationDisplay,
       DateDisplay
     },
     computed: {
+      ...mapGetters({
+        getCourse: 'courses/get'
+      }),
       publishedAt(){
         return this.video.published_at
       },
+      course(){
+        return this.getCourse(this.video.course_id)
+      },
+      partNumber(){
+        let sortedCourse = sortCourse(this.course, this.$store);
+        let index = sortedCourse.sortedItems.indexOf(this.video)
+        return index + 1
+      }
     },
     props: {
       video: {
