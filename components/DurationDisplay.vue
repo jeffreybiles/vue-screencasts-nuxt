d41bc6f98432f37533bd95d011384d8102d400df
Jeffrey Biles <bilesjeffrey@gmail.comgit config -l>
2019-12-11 14:35:20 -0800

    Display hours in DurationDisplay

--- components/DurationDisplay.vue
+++ components/DurationDisplay.vue
@@ -1,14 +1,22 @@
 <template>
   <span>
-    {{ minutes }}:{{ seconds }}
+    <span v-if="hours">{{hours}}:</span>{{ minutes }}:{{ seconds }}
   </span>
 </template>
 
 <script>
   export default {
     computed: {
+      hours(){
+        return Math.floor(this.duration / 3600)
+      },
       minutes(){
-        return Math.floor(this.duration / 60)
+        let minutes = Math.floor((this.duration % 3600) / 60);
+        let isSingleDigit = minutes.toString().length == 1
+        if(isSingleDigit && this.useHours){
+          minutes = `0${minutes}`
+        }
+        return minutes
       },
       seconds(){
         let seconds = this.duration % 60
@@ -17,12 +25,15 @@
           seconds = `0${seconds}`
         }
         return seconds
+      },
+      useHours(){
+        return this.duration > 60 * 60
       }
     },
     props: {
       duration: {
         type: Number
-      }
+      },
     }
   }
 </script>
