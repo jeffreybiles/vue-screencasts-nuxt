75c6c3f3457e962a9415b036c8278a883f97b7a7
Jeffrey Biles <bilesjeffrey@gmail.comgit config -l>
2019-12-13 15:00:40 -0800

    Sales copy on homepage

--- pages/index.vue
+++ pages/index.vue
@@ -1,14 +1,26 @@
 <template>
-  <div class="home">
-    <h1>Testimonials</h1>
+  <v-container class="home">
+    <!-- TODO: style this better so it takes up less room -->
+    <h1>Don't Be Basic</h1>
+    <p>There are dozens of great resources for getting started with VueJS...</p>
+    <p>But what comes next?</p>
+    <p>There are only so many times you can go over the basics before you're just wasting your time.</p>
+    <h1>Advanced Vue for Pro Developers</h1>
+    <p>Don't get the wrong idea... I'll still make some entry-level videos.</p>
+    <p>But my goal isn't to compete with all the other educators out there on who can explain v-bind the best.</p>
+    <p>Instead, I want to tackle meaty problems that Vue developers will encounter in the real world.</p>
+    
+    <h2>Near-daily releases start sometime in January</h2>
+
+
+    <!-- TODO: put in 6 courses -->
+    <hr>
+    <h1>Thousands have already learned from VueScreencasts</h1>
     <SocialProofGrid />
 
-    <div class="display-4 ma-4 d-flex justify-center">
-      All Videos
-    </div>
-    
+    <h1>Check out our newest videos</h1>
     <VideoTable :videos="videos" :headers="headers" />
-  </div>
+  </v-container>
 </template>
 
 <script>
