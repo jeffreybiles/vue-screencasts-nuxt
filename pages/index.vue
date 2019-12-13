80776f159b50b1cace14df8b40463d5928a6fd5d
Jeffrey Biles <bilesjeffrey@gmail.comgit config -l>
2019-12-13 15:39:25 -0800

    Display featured courses on home page

--- pages/index.vue
+++ pages/index.vue
@@ -9,14 +9,25 @@
     <p>Don't get the wrong idea... I'll still make some entry-level videos.</p>
     <p>But my goal isn't to compete with all the other educators out there on who can explain v-bind the best.</p>
     <p>Instead, I want to tackle meaty problems that Vue developers will encounter in the real world.</p>
-    
-    <h2>Near-daily releases start sometime in January</h2>
-
-
-    <!-- TODO: put in 6 courses -->
-    <hr>
+    <h1>Starting in late January 2020: new videos (almost) every workday</h1>
+    <p>Start your day off right by watching that morning's video, steadily improving your skills every day.</p>
+    <p>Or you could binge through the <em>more than sixteen hours</em> of tutorials I've already released.</p>
     <h1>Thousands have already learned from VueScreencasts</h1>
     <SocialProofGrid />
+    <hr>
+
+    <h1>Check out some of our courses</h1>
+    <v-row>
+      <v-col v-for="course in featuredCourses" 
+            :key="course.id"
+            cols="12" sm="6" lg="4">
+        <CourseCard :course="course" />
+      </v-col>
+    </v-row>
+
+
+    <hr>
+
 
     <h1>Check out our newest videos</h1>
     <VideoTable :videos="videos" :headers="headers" />
@@ -24,18 +35,35 @@
 </template>
 
 <script>
-import { mapState } from 'vuex';
+import { mapState, mapGetters } from 'vuex';
 import VideoTable from '@/components/VideoTable'
 import SocialProofGrid from '@/components/SocialProofGrid.vue';
+import CourseCard from '@/components/CourseCard.vue';
 
 export default {
   components: {
     VideoTable,
-    SocialProofGrid
+    SocialProofGrid,
+    CourseCard
+  },
+  data(){
+    return {
+      course_ids: [
+        3, //Building VueScreencasts.com
+        27, //Building Complex Data Tables with Vuetify's v-data-table and VueJS
+        15, //Introduction to NuxtJS
+        19, //Nuxt Auth: Authentication and Authorization in Nuxt
+        24, //Vue 2.6 Slots
+        26 //VueJS: Secure File Upload to S3, Directly from the Browser
+      ]
+    }
   },
   computed: {
     ...mapState({
       videos: state => state.videos.videos
+    }),
+    ...mapGetters({
+      getCourse: 'courses/get'
     }),
     headers(){
       return [
@@ -45,6 +73,9 @@ export default {
         {text: 'Duration', value: 'duration'},
         {text: 'Course', value: 'courseName'},
       ]
+    },
+    featuredCourses(){
+      return this.course_ids.map(c_id => this.getCourse(c_id))
     },
   }
 }
