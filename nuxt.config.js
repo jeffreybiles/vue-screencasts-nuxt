5084fc59d52eb90de71b5894ca4484e838ff49ab
Jeffrey Biles <bilesjeffrey@gmail.comgit config -l>
2019-12-19 21:19:01 -0800

    Google Analytics basic

--- nuxt.config.js
+++ nuxt.config.js
@@ -45,7 +45,14 @@ export default {
   buildModules: [
     // Doc: https://github.com/nuxt-community/eslint-module
     // '@nuxtjs/eslint-module'
-    '@nuxtjs/vuetify'
+    '@nuxtjs/vuetify',
+    ['@nuxtjs/google-analytics', {
+      id: 'UA-154843930-1',
+      debug: {
+        enabled: true,
+        sendHitTask: true
+      }
+    }]
   ],
   /*
   ** Nuxt.js modules
