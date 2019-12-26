57d91ea9a030295aca73d173fd8fb7d9134e58fc
Jeffrey Biles <bilesjeffrey@gmail.comgit config -l>
2019-12-26 04:47:09 -0600

    Mobile navbar

--- components/TheNavbar.vue
+++ components/TheNavbar.vue
@@ -1,21 +1,48 @@
 <template>
   <v-app-bar app color="green">
+    <!-- Home Page -->
+    <v-btn to="/" text>VueScreencasts</v-btn>
+
+    <!-- Desktop menu -->
     <v-btn v-for="link in navLinks"
-           :key="link.name"
-           text 
-           :to="link.to">
+          :key="link.name"
+          text 
+          :to="link.to"
+          class="hidden-sm-and-down">
       {{link.text}}
     </v-btn>
-    
     <v-spacer />
-    
     <v-btn v-for="link in authLinks"
-           :key="link.name"
-           text
-           :to="link.to"
-           @click="link.click ? link.click() : null">
+          :key="link.name"
+          text
+          right
+          :to="link.to"
+          class="hidden-sm-and-down"
+          @click="link.click ? link.click() : null">
       {{link.text}}
     </v-btn>
+
+    <!-- Mobile menu -->
+    <v-menu>
+      <template #activator="{on}">
+        <v-btn
+          icon
+          v-on="on"
+          class="hidden-md-and-up"
+        >
+          <v-icon>mdi-dots-vertical</v-icon>
+        </v-btn>
+      </template>
+      <v-list>
+        <v-list-item v-for="link in mobileLinks" 
+                     :key="link.text"
+                     :to="link.to"
+                     text
+                     @click="link.click ? link.click() : null">
+          <v-list-item-title>{{ link.text }}</v-list-item-title>
+        </v-list-item>
+      </v-list>
+    </v-menu>
   </v-app-bar>
 </template>
 
@@ -24,7 +51,6 @@
     computed: {
       navLinks(){
         let links = [
-          {text: "Vue Screencasts", to: "/"},
           {text: "Videos", to: "/videos"},
           {text: "Courses", to: "/courses"},
           {text: "Pro", to: "/pro"}
@@ -47,6 +73,10 @@
             {text: "Register", to: "/register"}
           ]
         }
+      },
+
+      mobileLinks(){
+        return this.navLinks.concat(this.authLinks)
       }
     }
   }
