9d02411dde78b72a5fa27db14200cd814d3f3553
Jeffrey Biles <bilesjeffrey@gmail.comgit config -l>
2019-12-26 11:33:54 -0600

    Trying more og items

--- pages/watch/_id.vue
+++ pages/watch/_id.vue
@@ -133,10 +133,12 @@ export default {
       title: title,
       meta: [
         {hid: 'og:title', name: 'og:title', property: 'og:title', content: title},
+        {hid: 'og:type', name: 'og:type', property: 'og:type', content: 'article'},
         {hid: 'og:url', name: 'og:url', property: 'og:url', content: `https://www.vuescreencasts.com/watch/${this.video.id}`},
         {hid: 'og:description', name: 'og:description', property: 'og:description', content: this.video.description},
         {hid: 'og:site_name', name: 'og:site_name', property: 'og:site_name', content: "VueScreencasts.com"},
         {hid: 'og:image', name: 'og:image', property: 'og:image', content: thumbnail},
+        {hid: 'og:image:url', name: 'og:image:url', property: 'og:image:url', content: thumbnail},
         {hid: 'og:image:width', name: 'og:image:width', property: 'og:image:width', content: 640},
         {hid: 'og:image:height', name: 'og:image:height', property: 'og:image:height', content: 360},
         {hid: 'og:image:type', name: 'og:image:type', property: 'og:image:type', content: "image/jpeg"},
