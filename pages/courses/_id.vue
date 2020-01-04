9498c3e301355662c31e04bc53845f266597a42b
Jeffrey Biles <bilesjeffrey@gmail.comgit config -l>
2020-01-04 11:13:42 -0800

    Start/Continue button on course page

--- pages/courses/_id.vue
+++ pages/courses/_id.vue
@@ -2,8 +2,20 @@
   <v-container>
     <v-row>
       <v-col cols="0" sm="4">
-        <v-img :src="course.image_url || ''" class="thumbnail-image" />
-        <DifficultyBars :difficulty="course.difficulty" :verbose="true" />
+        <v-img :src="course.image_url || ''" class="thumbnail-image mb-2" />
+
+        <v-row class="ma-0">
+          <DifficultyBars :difficulty="course.difficulty" :verbose="true" />
+          
+          <v-spacer />
+
+          <v-btn :to="`/watch/${firstUnplayedVideo.id}`" class="primary">
+            <span v-if="isComplete">Restart</span>
+            <span v-else-if="hasPlayedVideos">Continue</span>
+            <span v-else>Start</span>
+          </v-btn>
+        </v-row>
+
       </v-col>
       <v-col cols="12" sm="8">
         <h1>{{ course.name }}</h1>
@@ -22,7 +34,7 @@
   import { mapGetters } from 'vuex';
   import DurationDisplay from '@/components/DurationDisplay'
   import CourseContentTable from '@/components/CourseContentTable'
-  import courseDecorator from '../../utils/course-decorator';
+  import courseDecorator, { sortCourse, percentVideosComplete } from '../../utils/course-decorator';
   import MarkdownDisplay from '@/components/MarkdownDisplay.vue';
   import DifficultyBars from '@/components/DifficultyBars.vue';
 
@@ -41,6 +53,45 @@
         let course = this.getCourse(this.$route.params.id)
         return courseDecorator(course, this.$store);
       },
+      sortedVideos(){
+        let getVideo = this.$store.getters['videos/get']
+
+        let course = sortCourse(this.course, this.$store)
+
+        // this is a candidate for a utility (deep-sort?)
+        return course.sortedItems.map(i => {
+          if(i.video_ids) {
+            return i.video_ids.map(id => {
+              let video = getVideo(id)
+              return {...video, order: Number(i.order) * 100 + Number(video.order)}
+            })
+          } else {
+            return {...i, order: Number(i.order) * 100}
+          }
+        }).flat().sort((i, j) => {
+          return (i.order > j.order) ? 1 : -1
+        })
+      },
+      percentComplete(){
+        return percentVideosComplete(this.sortedVideos, this.$store);
+      },
+      hasPlayedVideos(){
+        return this.percentComplete > 0
+      },
+      isComplete(){
+        return this.percentComplete == 100
+      },
+      firstUnplayedVideo(){
+        let isPlayed = this.$store.getters['user/videoIsPlayed']
+
+        if(this.isComplete) {
+          return this.sortedVideos[0]
+        } else if(this.hasPlayedVideos) {
+          return this.sortedVideos.filter(v => !isPlayed(v.id))[0]
+        } else {
+          return this.sortedVideos[0]
+        }
+      }
     },
   }
 </script>
