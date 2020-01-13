6438dc171638bfc524ec7be591bfa8e3d230f977
Jeffrey Biles <bilesjeffrey@gmail.comgit config -l>
2020-01-13 15:34:35 -0800

    Better handling of cancelled/unsubscribed users

--- pages/account/billing.vue
+++ pages/account/billing.vue
@@ -1,56 +1,67 @@
 <template>
   <div>
     <h1>Billing</h1>
+
+    <span v-if="!subscription">
+      You are not subscribed.
+      <nuxt-link :to="`/order`">Order Now</nuxt-link>
+    </span>
+    <span v-else>
     
-    <p>
-      Your current card ends in <strong>{{card.last4}}</strong>.  
-      It expires <strong>{{card.exp_month}}/{{card.exp_year}}</strong>.
-    </p>
+      <p v-if="card">
+        Your current card ends in <strong>{{card.last4}}</strong>.  
+        It expires <strong>{{card.exp_month}}/{{card.exp_year}}</strong>.
+      </p>
+      <p v-else>
+        There is no card on file.  <nuxt-link :to="`/order`">Go to the order page</nuxt-link> to restart your account.
+      </p>
 
-    <p>
-      <!-- TODO: make sure this displays correctly once user has unsubscribed -->
-      <span v-if="subscription.cancel_at_period_end">
-        Your subscription will end on
-        <!-- TODO: make a one-click way for them to resubscribe -->
-      </span>
-      <span v-else>
-        Your next payment will be on 
-      </span>
-      {{ new Date(subscription.current_period_end * 1000).toDateString() }}
-    </p>
-
-    <h2 class="mt-3">Previous Payments</h2>
-    <v-simple-table>
-      <thead>
-        <tr>
-          <th>Date</th>
-          <th>Amount</th>
-          <th>Paid?</th>
-          <th>Card</th>
-          <th>Invoice</th>
-        </tr>
-      </thead>
-      <tbody>
-        <tr v-for="charge in charges" :key="charge.id">
-          <td>{{ new Date(charge.created * 1000).toDateString() }}</td>
-          <td>${{ charge.amount / 100 }}</td>
-          <td>{{charge.paid}}</td>
-          <td>{{charge.payment_method_details.card.last4}}</td>
-          <td><a :href="`${charge.receipt_url}`" target="_blank">Link</a></td>
-        </tr>
-      </tbody>
-    </v-simple-table>
-
-    <span v-if="!subscription.cancel_at_period_end">
-      <h2 class="mt-3">Cancellation</h2>
-      <p>Switched frameworks?  Had enough learning?</p>
-      <v-btn color="red" @click="cancelSubscription" :disabled="cancelActionIsLoading">
-        Cancel Subscription 
-        <span v-if="cancelActionIsLoading">
-          &nbsp;
-          <v-progress-circular indeterminate  size="20" />
+      <p>
+        <span v-if="!$auth.user.pro">
+          You are not currently subscribed.
         </span>
-      </v-btn>
+        <span v-else-if="subscription.canceled_at">
+          Your subscription will end on {{ subscriptionEndDate }}
+          <!-- TODO: make a one-click way for them to resubscribe -->
+        </span>
+        <span v-else>
+          Your next payment will be on {{ subscriptionEndDate }}
+        </span>
+      </p>
+
+      <h2 class="mt-3">Previous Payments</h2>
+      <v-simple-table>
+        <thead>
+          <tr>
+            <th>Date</th>
+            <th>Amount</th>
+            <th>Paid?</th>
+            <th>Card</th>
+            <th>Invoice</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr v-for="charge in charges" :key="charge.id">
+            <td>{{ new Date(charge.created * 1000).toDateString() }}</td>
+            <td>${{ charge.amount / 100 }}</td>
+            <td>{{charge.paid}}</td>
+            <td>{{charge.payment_method_details.card.last4}}</td>
+            <td><a :href="`${charge.receipt_url}`" target="_blank">Link</a></td>
+          </tr>
+        </tbody>
+      </v-simple-table>
+
+      <span v-if="!subscription.canceled_at">
+        <h2 class="mt-3">Cancellation</h2>
+        <p>Switched frameworks?  Had enough learning?</p>
+        <v-btn color="red" @click="cancelSubscription" :disabled="cancelActionIsLoading">
+          Cancel Subscription 
+          <span v-if="cancelActionIsLoading">
+            &nbsp;
+            <v-progress-circular indeterminate  size="20" />
+          </span>
+        </v-btn>
+      </span>
     </span>
   </div>
 </template>
@@ -65,6 +76,11 @@
         card,
         subscription,
         cancelActionIsLoading: false
+      }
+    },
+    computed: {
+      subscriptionEndDate(){
+        return new Date(this.subscription.current_period_end * 1000).toDateString()
       }
     },
     methods: {
