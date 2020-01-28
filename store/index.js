69f6d49e8e0259aba75e929f88a6c3ef1c2bbe99
Jeffrey Biles <bilesjeffrey@gmail.comgit config -l>
2020-01-27 22:21:19 -0800

    only load data once
    Previous method, with `from`, only worked in Universal mode... and it worked because `from` isn't available in SSR mode (aka initial load)

--- store/index.js
+++ store/index.js
@@ -1,4 +1,13 @@
-
 export const strict = false
+
+export const state = () => ({
+  isLoaded: false,
+})
+
+export const mutations = {
+  FINISH_LOADING(state) {
+    state.isLoaded = true;
+  }
+}
 
 // Oh well... if it's good enough for VueX default, it's good enough for us.
