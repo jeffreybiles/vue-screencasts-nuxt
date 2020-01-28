f0b42a6126c1b8686b65f9220bdd3138a6f08470
Jeffrey Biles <bilesjeffrey@gmail.comgit config -l>
2020-01-28 02:07:28 -0800

    Mirage - BaseSerializer and keyForAttribute + add duration, codeSummary, published_at

--- plugins/miragejs.js
+++ plugins/miragejs.js
@@ -1,4 +1,5 @@
 import { Server, Model, JSONAPISerializer, hasMany } from 'miragejs';
+import _ from 'lodash';
 
 let videos = [
   {
@@ -7,6 +8,9 @@ let videos = [
     description: `<p>ES2015 (aka ES6) has some great ways to make your code easier to write and understand. In this episode, we cover two different ways that you can make your code clearer by removing the 'function' keyword.</p>`,
     thumbnail: "https://vue-screencasts.s3.us-east-2.amazonaws.com/images/video-thumbnails/Thumbnail+-+Arrow+Function.png",
     videoUrl: "https://vue-screencasts.s3.us-east-2.amazonaws.com/video-files/38-+es2015-+functions+minus+'function'.mp4",
+    duration: '300',
+    codeSummary: `let code = 'coolCode'`,
+    published_at: new Date(),
     tagIds: ['1', '2'],
   },
   {
@@ -16,9 +20,18 @@ let videos = [
                   <p>Here are 3 cool things that template strings enable.</p>`,
     thumbnail: "https://vue-screencasts.s3.us-east-2.amazonaws.com/images/video-thumbnails/Thumbnail+-+Template+Strings.png",
     videoUrl: "https://vue-screencasts.s3.us-east-2.amazonaws.com/video-files/42-+ES2015+template+strings.mp4",
+    duration: '236',
+    code_summary: 'let code = "This code is even cooler."',
+    published_at: Date.now(),
     tagIds: ['1'],
   }
 ];
+
+let BaseSerializer = JSONAPISerializer.extend({
+  keyForAttribute(attr) {
+    return _.snakeCase(attr);
+  }
+});
 
 let tags = [{
   id: '1',
@@ -59,8 +72,8 @@ new Server({
     user: Model,
   },
   serializers: {
-    application: JSONAPISerializer,
-    tag: JSONAPISerializer.extend({
+    application: BaseSerializer,
+    tag: BaseSerializer.extend({
       include: ['videos'],
       normalize(json) {
         return {
@@ -71,7 +84,7 @@ new Server({
         }
       }
     }),
-    video: JSONAPISerializer.extend({
+    video: BaseSerializer.extend({
       include: ['tags'],
       normalize(json) {
         return {
