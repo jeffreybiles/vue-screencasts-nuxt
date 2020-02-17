fc1f3608f14c3728b3d929b178d7cfb853bb34cf
Jeffrey Biles <bilesjeffrey@gmail.comgit config -l>
2020-02-16 18:38:07 -0800

    4th section

--- pages/index.vue
+++ pages/index.vue
@@ -19,9 +19,9 @@
 
     <section class="section" id="why-vue">
       <div class="paper-container-large">
-        <div class="row">
+        <div class="row vertical-align-items">
           <div class="col-md-6 col-sm-12">
-            <h2 class="section-title margin-sm">Why VueScreencasts?</h2>
+            <h2 class="section-title">Why VueScreencasts?</h2>
             <p>You're beyond the first two hours.</p>
             <p>Watching the same type of intro tutorials over and over isn't getting you anywhere.</p>
             <p>But maybe you don't have access to an expert... or the experts you know prefer coding to teaching.</p>
@@ -41,12 +41,12 @@
 
     <section class="section bg-gray">
       <div class="paper-container-large">
-        <div class="row">
-          <div class="col-md-6 col-sm-12 center-md">
+        <div class="row vertical-align-items">
+          <div class="col-md-5 col-sm-12 center-md">
             <img src="~assets/vuescreencasts-student-coding.png" class="img-responsive" alt="VueScreencasts Student Coding">
           </div>
-          <div class="col-md-6 col-sm-12">
-            <h2 class="section-title margin-lg">What will I be getting?</h2>
+          <div class="col-md-7 col-sm-12">
+            <h2 class="section-title">What will I be getting?</h2>
             <p>Wherever you are in your Vue career, we have a package to meet your needs.</p>
             <p>Some people will benefit from weekly coaching meetings to keep them on track.</p>
             <p>Others will be satisfied with our monthly in-depth courses and our huge library of existing videos.</p>
@@ -57,9 +57,40 @@
       </div>
     </section>
 
+    <!-- TODO: choose which testimonials to put here -->
+    <div class="testimonial-color">
+      <TestimonialsRow :testimonialIds="['1', '2', '3', '4']" class="paper-container-large" />
+    </div>
+
+    <section class="section bg-dark">
+      <div class="paper-container-large">
+        <div class="row vertical-align-items">
+          <div class="col-md-7 col-sm-12">
+            <h2 class="section-title">Are you ready?</h2>
+            <p>Remember, we have a 30-day 100% money-back guarantee, and at any point you can easily cancel or change your subscription level with just one email.</p>
+            <p>If you have any questions, feel free to contact us.</p>
+            <ul>
+              <li>Email: <a href="mailto:jeffrey@vuescreencasts.com" target="_blank">jeffrey@vuescreencasts.com</a></li>
+              <li>Twitter: <a href="https://twitter.com/vuescreencasts" target="_blank">@vuescreencasts</a></li>
+              <li>Text or Phone: <a href="sms:501-256-3078">+1 (501) 256-3078</a></li>
+            </ul>
+          </div>
+          <div class="col-md-5 col-sm-12 center-md">
+            <img src="~assets/vuescreencasts-student-coding-2.png" class="img-responsive img-center-xs" alt="Ember School Student Learning">
+          </div>
+        </div>
+      </div>
+    </section>
 
 
-      <!-- <h2 class="section-title margin-lg">Pro Platinum - for when there's money on the line, and you need to deliver.</h2>
+    <!-- TODO: choose which testimonials to put here -->
+    <div class="testimonial-color">
+      <TestimonialsRow :testimonialIds="['1', '2', '3', '4']" class="paper-container-large" />
+    </div>
+
+
+
+      <!-- <h2 class="section-title">Pro Platinum - for when there's money on the line, and you need to deliver.</h2>
     <p>When you need an expert on your side, this is the package for you.</p>
     <p>We'll start with a comprehensive analysis of your situation and come up with a custom action plan to get you where you need to be.</p>
     <p>Then we'll keep you on track with weekly meetings.</p>
@@ -112,6 +143,17 @@ export default {
 
 <style lang="scss">
 $font: Roboto, sans-serif;
+
+$indigo: #3F51B5;
+$indigo-300: #7986CB;
+$indigo-100: #c5cae9;
+$orange: #FF6E40;
+$grey: #E2E7E9;
+$dark-grey: #333;
+$navy: #24303C;
+$dark-navy: #1A242E;
+$green: #23B408;
+
 
 $blue: #2196f3;
 $blue-300: #64b5f6;
@@ -209,21 +251,6 @@ $primary: $blue;
   .section-title {
     font: 300 32px/40px $font;
     margin-bottom: 30px;
-    &.margin-sm {
-      @media (min-width: 75em) {
-        margin-top: 60px;
-      }
-    }
-    &.margin-lg {
-      @media (min-width: 75em) {
-        margin-top: 100px;
-      }
-    }
-    &.margin-xl {
-      @media (min-width: 75em) {
-        margin-top: 120px;
-      }
-    }
   }
   ul {
     margin: 0;
@@ -238,5 +265,38 @@ $primary: $blue;
     margin-bottom: -66px;
   }
 }
+
+.bg-dark {
+  background-color: #2D384A;
+  .section-title {
+    color: #ffffff;
+  }
+  color: #ffffff;
+  h2, h3, h4 {
+    color: #ffffff;
+  }
+  button.md-button.md-default-theme.md-raised:disabled {
+    background-color: $grey;
+  }
+  .disabled {
+    color: #aaaaaa;
+  }
+  .check {
+    color: $green;
+  }
+  li a {
+    color: #ffffff;
+  }
+}
+
+.img-responsive {
+  max-width: 100%;
+}
+
+  .vertical-align-items {
+    display: flex;
+    justify-content: center; /* align horizontal */
+    align-items: center;
+  }
 
 </style>
