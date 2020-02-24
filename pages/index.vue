bbb232a6c2d5158d31fa8c84c8cba0800687d2eb
Jeffrey Biles <bilesjeffrey@gmail.comgit config -l>
2020-02-24 10:37:14 -0800

    Rework language in home page

--- pages/index.vue
+++ pages/index.vue
@@ -10,19 +10,25 @@
       <div class="col-md-6 col-sm-5"></div>
     </HomePageSection>
 
-    <!-- TODO: choose which testimonials to put here -->
     <div class="bg-gray">
-      <TestimonialsRow :testimonialIds="['12', '21', '4', '34']" class="paper-container-large" />
+      <TestimonialsRow :testimonialIds="['100', '21', '65', '27']" class="paper-container-large" />
     </div>
 
     <HomePageSection sectionId="why-vue">
       <div class="col-md-6 col-sm-12">
-        <h2 class="section-title">Why VueScreencasts?</h2>
-        <p>You're beyond the first two hours.</p>
+        <h2 class="section-title">Remember how Neo downloaded Kung Fu? This is as close as it gets (but for Vue).</h2>
+
+        <!-- Can combine these two -->
+        <p>You've seen your friends flounder around, watching free tutorial after free tutorial, but never getting beyond the beginner level.</p>
+        <p>You've seen other coders go down wrong paths, use bad patterns, and generally make a mess of things.</p>
+
+        <p>There's nothing wrong with starting with free resources... this is your next step to developing true expertise.</p>
+
+        <!-- 
         <p>You know that Vue is a power tool that can make your coding life so much simpler, letting you solve issues with ease...</p>
         <p>But it's hard to level up your skills.  Watching the same type of intro tutorials over and over isn't getting you anywhere.</p>
         <p>Maybe you don't have access to an expert... or the experts you know prefer coding to teaching.</p>
-        <p>Good news: we're here, ready to help you level up your Vue skills.  Call or text us any time (501-256-3078), or email us (<a href="mailto:jeffrey@vuescreencasts.com" target="_blank">jeffrey@vuescreencasts.com</a>), or message us on Twitter (<a href="https://twitter.com/vuescreencasts" target="_blank">@vuescreencasts</a>) and let's talk about building your future.</p>
+        <p>Good news: we're here, ready to help you level up your Vue skills.  Call or text us any time (501-256-3078), or email us (<a href="mailto:jeffrey@vuescreencasts.com" target="_blank">jeffrey@vuescreencasts.com</a>), or message us on Twitter (<a href="https://twitter.com/vuescreencasts" target="_blank">@vuescreencasts</a>) and let's talk about building your future.</p> -->
       </div>
       <div class="col-md-6 col-sm-12 center-md">
         <img src="~assets/happy-vue-programmer.png" class="img-responsive bottom-image" alt="Happy Vue Programmer">
@@ -39,12 +45,17 @@
         <img src="~assets/vuescreencasts-student-coding.png" class="img-responsive" alt="VueScreencasts Student Coding">
       </div>
       <div class="col-md-7 col-sm-12">
-        <h2 class="section-title">You don't have to do it all yourself</h2>
-        <p>Have you ever banged your head against a wall for days, maybe even weeks, struggling to learn something new... only for it to click into place the second you heard the right metaphor?</p>
+        <h2 class="section-title">Why keep guessing?</h2>
+        <p>Imagine going into a gym, and they hand you a book.  "Here are the exercises." It's three hundred pages of detailed instructions about every exercise you could possibly perform in that gym.</p>
+        <p>Even if it's the best-written book in the world, even if the book has an 'example routines' section at the beginning, most people will still quit.</p>
+        <p>Now imagine walking in, and you have a personal trainer that designs a routine specifically for you. No more guessing at what exercise you should do, how many reps, when to push and when to slow down. All you have to do is show up and put in the work.</p>
+        <p>We're your personal trainer for Vue.</p>
+
+        <!-- <p>Have you ever banged your head against a wall for days, maybe even weeks, struggling to learn something new... only for it to click into place the second you heard the right metaphor?</p>
         <p>Maybe you're the first person in your company to encounter a specific issue, and the friendly people over at StackOverflow tell you to RTFM... but there's no manual.</p>
         <p>What now?</p>
         <p>Whether you need personalized, on-demand assistance, weekly coaching calls to keep you on track, or just a friendly voice to help you along, we've got a package for you.</p>
-        <p>We'll meet you wherever you're at, and do whatever we can to get you one level higher.</p>
+        <p>We'll meet you wherever you're at, and do whatever we can to get you one level higher.</p> -->
       </div>
     </HomePageSection>
 
@@ -118,7 +129,7 @@
         <ProPackageCard :stripePlanId="3">
           <p>A little push may be all you need to get yourself on a better trajectory.</p>
           <p>Monthly meetings, weekly check-ins...</p>
-          <p>The results are slow in building, but this is an investment that will pay for itself many times over the course of your career.</p>
+          <p>The results may be slower than in our other plans, but this is an investment that will pay for itself many times over the course of your career.</p>
         </ProPackageCard>
       </div>
     </HomePageSection>
@@ -134,7 +145,7 @@
 
         <ProPackageCard :stripePlanId="6">
           <p>This is for the people who don't like seeking help... but know, deep down, that mentorship can make them an even better version of themselves.</p>
-          <p>Weekly email check-ins give you the chance to get the guidance you need, and you'll never have to speak up or show your face.</p>
+          <p>Weekly email check-ins give you the chance to get the guidance you need, without having to ever show up for a meeting.</p>
         </ProPackageCard>
       </div>
     </HomePageSection>
