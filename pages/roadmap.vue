8bfd11d0640118d81145e1de4e38ff99a8b3b689
Jeffrey Biles <bilesjeffrey@gmail.comgit config -l>
2020-02-25 09:18:08 -0800

    Update roadmap

--- pages/roadmap.vue
+++ pages/roadmap.vue
@@ -1,22 +1,7 @@
 <template>
   <v-container>
-    <h1>Videos</h1>
-
-    <p>New videos every week... every day, if I can manage it.</p>
-    <p>Here's a tentative short-term schedule:</p>
-    <ul>
-      <li>Jan. 16th - VueScreencasts Preview</li>
-      <li>Jan. 17th - Removing Tags</li>
-      <li>Jan. 18th - VueX Strict Mode</li>
-      <li>Jan. 19th - Data Structure for Courses</li>
-      <li>We'll continue to build VueScreencasts.com in the free courses</li>
-    </ul>
-    <br>
-    <p>Pro screencasts will cover topics in even more depth.  Currently researching, first pro series coming soon.</p>
-
     <h1>Upgrades/Features</h1>
     <ul>
-      <li>Refresh visual design</li>
       <li>Improve thumbnails for courses</li>
       <li>"Watched" list</li>
       <li>"Watch Later" list</li>
