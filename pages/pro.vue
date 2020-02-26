c9262d338e509883f6f39e88e673a7fe8872561b
Jeffrey Biles <bilesjeffrey@gmail.comgit config -l>
2020-02-25 18:09:57 -0800

    Make the back button work again

--- pages/pro.vue
+++ pages/pro.vue
@@ -6,7 +6,7 @@
 <script>
   export default {
     created(){
-      this.$router.push(`/#${ this.$route.query.solution || 'pro-platinum'}`);
+      this.$router.replace(`/#${ this.$route.query.solution || 'pro-platinum'}`);
     }
   }
 </script>
