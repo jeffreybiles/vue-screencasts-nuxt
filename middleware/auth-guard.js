dc974b4a537da9391295ce769cd4e63417d1681e
deadw00d <pure.pudge@gmail.com>
2020-04-17 23:19:30 +0700

    Auth guard back redirect URL

--- middleware/auth-guard.js
+++ middleware/auth-guard.js
@@ -1,6 +1,8 @@
-export default async function({$auth, redirect, store}) {
+export default async function({$auth, route, redirect, store}) {
   if(!$auth.user) {
+    const REDIRECT_URL = '/login?redirect=' + route.path
     store.dispatch('snackbar/setSnackbar', {color: 'error', text: "You must be logged in to view that page."})
-    redirect('/login')
+    redirect(REDIRECT_URL)
   }
 }
+
