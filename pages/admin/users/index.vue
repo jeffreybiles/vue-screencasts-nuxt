5a33ad84b62c865c987f339f0509521fba3a34fd
deadw00d <pure.pudge@gmail.com>
2020-04-26 18:38:44 +0700

    Added users table

--- pages/admin/users/index.vue
+++ pages/admin/users/index.vue
@@ -10,12 +10,34 @@
     <h1 v-if="filterState">Users whose plan is: {{filterState.name}}</h1>
     <h1 v-else>All users</h1>
 
+    <v-text-field
+      v-model="search"
+      single-line
+      hide-details>
+      <template #label>
+        &nbsp; Search by name or email among {{filteredUsers.length}} users
+      </template>
+    </v-text-field>
+    <v-data-table :items="filteredUsers"
+                  :headers="headers"
+                  :search="search"
+                  :custom-filter="filter"
+    >
+      <template #item.pro="{item}">
+        <v-icon v-if="item.pro" color="green">mdi-check-circle</v-icon>
+        <v-icon v-else color="red">mdi-cancel</v-icon>
+      </template>
+      <template #item.plan_id="{item}">
+        {{ item.plan_id ? getPlanWithDefault(item.plan_id).name : ' – ' }}
+      </template>
+    </v-data-table>
   </div>
 </template>
 
 <script>
   import subscriptionPlanJson from '@/utils/subscription-plan-data.json'
   import { mapState } from 'vuex';
+  import {getPlanWithDefault} from "~/utils/subscription-utils";
 
   export default {
     async middleware({store}) {
@@ -25,6 +47,7 @@
     },
     data(){
       return {
+        search: '',
         filterState: null,
         plans: subscriptionPlanJson.plans
       }
@@ -37,6 +60,9 @@
         return this.getPlanIds(this.filterState)
       },
       filteredUsers(){
+        if (!this.filterState) {
+          return this.users
+        }
         return this.users.filter(u => {
           return this.currentFilterPlanIds.includes(u.plan_id)
         })
@@ -44,16 +70,15 @@
       headers(){
         return [
           {text: 'Name', value: 'name'},
+          {text: 'Email', value: 'email'},
           {text: 'Pro', value: 'pro', width: '70px'},
-          {text: 'Date', value: 'sortable_published_at'},
-          {text: 'Duration', value: 'duration'},
-          {text: 'Course', value: 'courseName'}
+          {text: 'Plan', value: 'plan_id'}
         ]
       },
     },
     methods: {
       getPlanIds(plan) {
-        if (!plan.month || !plan.year) {
+        if (!plan || !plan.month || !plan.year) {
           return []
         }
         return [
@@ -62,7 +87,14 @@
           plan.year.stripeId.dev,
           plan.year.stripeId.prod
         ]
-      }
+      },
+      filter(value, search, item) {
+        let isInName = (item) => RegExp(search, 'i').test(item.name)
+        let isInEmail = (item) => RegExp(search, 'i').test(item.email)
+
+        return isInName(item) || isInEmail(item)
+      },
+      getPlanWithDefault
     }
   }
 </script>
