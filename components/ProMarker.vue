02b4db1631529f164fe889189079ed57f0b0d9ea
Jeffrey Biles <bilesjeffrey@gmail.comgit config -l>
2020-04-26 00:12:40 -0700

    Make ProMarker always locked if not pro

--- components/ProMarker.vue
+++ components/ProMarker.vue
@@ -3,13 +3,14 @@
     <slot name="free"></slot>
   </span>
   <span v-else>
-    <slot name="pro" :canAccess="canAccess">
-      <span v-if="canAccess">
-        <slot name="unlocked"><font-awesome-icon icon="lock-open" /></slot>
-      </span>
-      <span v-else>
+    <slot name="pro">
+      <span v-if="isPro">
         <slot name="locked"><font-awesome-icon icon="lock" /></slot>
       </span>
+      <span>
+        <slot name="unlocked"><font-awesome-icon icon="lock-open" /></slot>
+      </span>
+      
     </slot>
   </span>
 </template>
@@ -17,9 +18,9 @@
 <script>
   export default {
     computed: {
-      canAccess(){
+      isPro(){
         let user = this.$auth.user
-        return this.video.in_free_period || user && user.pro
+        return user && user.pro
       },
     },
     props: {
