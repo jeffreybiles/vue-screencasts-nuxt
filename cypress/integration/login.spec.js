031b6e11cdb8445a8b24bdf42a61c4f56213e149
deadw00d <pure.pudge@gmail.com>
2020-05-03 13:37:32 +0700

    Removed requests mocks from login test Added test for login with incorrect password

--- cypress/integration/login.spec.js
+++ cypress/integration/login.spec.js
@@ -1,46 +1,58 @@
 const loginUrl = '/api/sessions';
 const userUrl = '/api/sessions/user';
 
-function login() {
+function login(user, password) {
   cy.visit('/auth');
-  cy.get('input[type=text]').type('admin@user.com');
-  cy.get('input[type=password]').type('aaaaaaaa');
+  cy.get('input[type=text]').type(user);
+  cy.get('input[type=password]').type(password);
   cy.get('button#authButton').click();
 }
 
-function mockLoginResponse() {
-  cy.server();
-  cy.fixture('user-login-success.json').as('userLoginSuccessJson');
-  cy.fixture('user-details.json').as('userDetailsJson');
-  cy.route({
-    method: 'POST',
-    url: loginUrl,
-    response: '@userLoginSuccessJson',
-  }).as('mockLoginSuccess');
-  cy.route({
-    method: 'GET',
-    url: userUrl,
-    response: '@userDetailsJson',
+context('User Authorization', () => {
+
+  describe('Success login', () => {
+    it('is logged out', () => {
+      cy.visit('/')
+        .window()
+        .its('app.store')
+        .its('state.auth.loggedIn')
+        .should('equal', false);
+    });
+
+    it('can log in', () => {
+      login('admin@user.com', 'aaaaaaaa');
+
+      cy.window()
+        .its('app.store')
+        .its('state.auth.loggedIn')
+        .should('equal', true);
+    });
+
+    it('logged in', () => {
+      cy.contains('header', 'admin@user.com');
+      cy.contains('header', 'Logout');
+    });
   });
-}
 
-describe('User', () => {
-  it('is logged out', () => {
-    cy.visit('/')
-      .window()
-      .its('app.store')
-      .its('state.auth.loggedIn')
-      .should('equal', false);
+  describe('Failed login with incorrect password', () => {
+    it('is logged out', () => {
+      cy.visit('/')
+        .window()
+        .its('app.store')
+        .its('state.auth.loggedIn')
+        .should('equal', false);
+    });
+
+    it('failed to login', () => {
+      login('admin@user.com', 'aaaaaaa1');
+
+      cy.window()
+        .its('app.store')
+        .its('state.auth.loggedIn')
+        .should('equal', false);
+
+      cy.contains('There was an issue');
+    });
   });
 
-  it('can log in', () => {
-    mockLoginResponse();
-
-    login();
-
-    cy.window()
-      .its('app.store')
-      .its('state.auth.loggedIn')
-      .should('equal', true);
-  });
-});
+})
