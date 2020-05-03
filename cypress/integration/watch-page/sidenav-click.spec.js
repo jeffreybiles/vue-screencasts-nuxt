58967f13b42ac9320131b88af381d7175a74fa74
deadw00d <pure.pudge@gmail.com>
2020-05-03 23:38:51 +0700

    Added watch-page sidenav test

---
+++ cypress/integration/watch-page/sidenav-click.spec.js
@@ -0,0 +1,13 @@
+describe('Continue course watch', () => {
+
+  it('navigate to /watch/20', () => {
+    cy.visit('/watch/20')
+  })
+
+  it('navigates to another video with sidenav click', () => {
+    cy.get('.scroll-box .video-row:not(.active)').first().click()
+    cy.url().should('not.match', /watch\/20/)
+  })
+
+
+});
