c80afeaed656b33d8cb0d374fd168a218de4357b
Jeffrey Biles <bilesjeffrey@gmail.comgit config -l>
2020-05-07 13:30:46 -0700

    Spinner on registration

---
+++ components/SpinnerIfLoading.vue
@@ -0,0 +1,30 @@
+<template>
+  <div>
+    <div v-if="loading">
+      <spinner size="large" class="mt-4" />
+    </div>
+    <div v-else>
+      <slot />
+    </div>
+  </div>
+</template>
+
+<script>
+  import Spinner from 'vue-simple-spinner/src/components/Spinner.vue'
+
+  export default {
+    components: {
+      Spinner
+    },
+    props: {
+      loading: {
+        type: Boolean,
+        default: false
+      }
+    }
+  }
+</script>
+
+<style lang="scss" scoped>
+
+</style>
--- components/UserAuthTogglableForm.vue
+++ components/UserAuthTogglableForm.vue
@@ -2,35 +2,45 @@
   <div>
     <div v-if="authScreen == 'login'">
       <h1>{{ loginPhrase }}</h1>
-      <p>New User? <a @click="changeScreen('register')">Click here to register.</a></p>
-      <UserAuthForm buttonText="Login" :submitForm="loginUser" />
+
+      <SpinnerIfLoading :loading="loading">
+        <p>New User? <a @click="changeScreen('register')">Click here to register.</a></p>
+        <UserAuthForm buttonText="Login" :submitForm="loginUser" />
+      </SpinnerIfLoading>
     </div>
     <div v-else>
       <h1>{{ registerPhrase }}</h1>
-      <p>Existing User? <a @click="changeScreen('login')">Click here to login.</a></p>
-      <UserAuthForm buttonText="Register" 
-          :submitForm="registerUser" 
-          :hasName="true" 
-          :registrationCheckboxes="true" />
+      <SpinnerIfLoading :loading="loading">
+        <p>Existing User? <a @click="changeScreen('login')">Click here to login.</a></p>
+        <UserAuthForm buttonText="Register" 
+            :submitForm="registerUser" 
+            :hasName="true" 
+            :registrationCheckboxes="true" />
+      </SpinnerIfLoading>
     </div>
   </div>
 </template>
 
 <script>
   import UserAuthForm from '@/components/UserAuthForm.vue';
+  import SpinnerIfLoading from '@/components/SpinnerIfLoading.vue';
 
   export default {
     data(){
       let authScreen = this.$route.query.authScreen || this.startingScreen
       return {
-        authScreen
+        authScreen,
+        loading: false
       }
     },
     components: {
-      UserAuthForm
+      UserAuthForm,
+      SpinnerIfLoading,
     },
     methods: {
       async loginUser(loginInfo){
+        this.loading = true;
+
         try {
           await this.$auth.loginWith('local', {
             data: loginInfo
@@ -41,8 +51,12 @@
         } catch {
           this.$store.dispatch('snackbar/setSnackbar', {color: 'red', text: 'There was an issue signing in.  Please try again.'})
         }
+
+        this.loading = false;
       },
       async registerUser(registrationInfo){
+        this.loading = true;
+
         try {
           await this.$axios.post('/users', registrationInfo)
 
@@ -55,6 +69,8 @@
         } catch {
           this.$store.dispatch('snackbar/setSnackbar', {color: 'red', text: 'There was an issue signing up.  Please try again.'})
         }
+
+        this.loading = false;
       },
       changeScreen(newScreen){
         this.authScreen = newScreen
